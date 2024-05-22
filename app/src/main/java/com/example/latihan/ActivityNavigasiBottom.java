dceb06af634a72dfe957411af773c6caf66cc076
Yusuf-Rizqy-M <yusufhabib290@gmail.com>
2024-05-22 10:07:49 +0700

    Initial commit

---
+++ app/src/main/java/com/example/latihan/ActivityNavigasiBottom.java
@@ -0,0 +1,108 @@
+package com.example.latihan;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
+
+import fragmentnavigasibottom.api_pertama;
+import fragmentnavigasibottom.api_kedua;
+
+
+public class ActivityNavigasiBottom extends AppCompatActivity {
+
+    private BottomNavigationView bottomNavigationView;
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle toggle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_navigasi_bottom);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(
+                this, drawerLayout, toolbar, R.string.open_drawer, R.string.close_drawer);
+
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();  // Sync the state of the toggle
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.api1) {
+                    loadFragment(new api_pertama(), false);
+                } else if (itemId == R.id.api2) {
+                    loadFragment(new api_kedua(), false);
+                } else {
+                    loadFragment(new ProfileFragment(), false);
+                }
+
+                drawerLayout.closeDrawers();
+                return true;
+            }
+        });
+
+        bottomNavigationView = findViewById(R.id.bottomNavView);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.api1) {
+                    loadFragment(new api_pertama(), false);
+                } else if (itemId == R.id.api2) {
+                    loadFragment(new api_kedua(), false);
+                } else {
+                    loadFragment(new ProfileFragment(), false);
+                }
+                return true;
+            }
+        });
+
+        loadFragment(new api_pertama(), true);
+    }
+
+    private void loadFragment(Fragment fragment, boolean isAppInitialized) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (isAppInitialized) {
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+
+        fragmentTransaction.commit();
+    }
+
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        toggle.syncState();  // Ensure the drawer toggle is synchronized
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
